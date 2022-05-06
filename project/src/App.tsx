ee61d789bb8a4e1cbc80865344210fb240c5dbb2
Lucas Almeida <lucas.almeida@qesh.ai>
2022-05-06 13:41:55 -0300

    initial commit. splash adicionada. :sparkles:

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -4,10 +4,12 @@ import {Image, View} from 'react-native';
 const App: React.FC = () => {
   return (
     <View
+      // eslint-disable-next-line react-native/no-inline-styles
       style={{
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: '#d41d1d',
       }}>
       <Image source={require('./assets/logo.png')} />
     </View>
